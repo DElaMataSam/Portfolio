67ba84c7034e9776c49896855e2b67733fb2cffc
DeLaMataSAM <122674997+DElaMataSam@users.noreply.github.com>
2024-01-12 13:09:06 +0100

    Update Query_explamples.sql

--- SQL Database creation/Query_explamples.sql
+++ SQL Database creation/Query_explamples.sql
@@ -8,6 +8,6 @@ SELECT COUNT(*) FROM factura;
 
 SELECT SUM(cantidad) FROM detalle_factura;
 
-SELECT  SUM(df.cantidad), TO_CHAR(f.fecha, 'DD/MM/YYYY') FROM detalle_factura df, factura f INNER JOIN df.idFactura = f.idFactura 
+SELECT  SUM(df.cantidad), TO_CHAR(f.fecha, 'DD/MM/YYYY') FROM detalle_factura df INNER JOIN fechas f ON df.idFactura = f.idFactura 
 GROUP BY TO_CHAR(f.fecha, 'DD/MM/YYYY') 
 ORDER BY TO_DATE(TO_CHAR(f.fecha, 'DD/MM/YYYY', 'DD/MM/YYYY'));
